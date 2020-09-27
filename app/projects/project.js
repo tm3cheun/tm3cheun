bd82afc7a00916e5ea6601449160bebcd071ed3a
Tiffany <tiffany.cheung23@gmail.com>
2020-09-27 10:12:51 -0400

    Fix broken project page and update layouts for better design

--- app/projects/project.js
+++ app/projects/project.js
@@ -12,4 +12,30 @@ angular.module('myApp.project', ['ngRoute'])
 		});
 	}
 	$scope.init();
+
+	$scope.initAnimateScroll = function () {
+		var offset = 50;
+
+    $(function ($) {
+	    // wait till load event fires so all resources are available
+	    $scope.$section = $('a.scroll').click(function(e) {
+      	var hash = e.target.hash.split("#");
+      	hash = hash[hash.length-1];
+
+      	var obj = $("#" + hash);
+
+      	if (obj.offset()) {
+			    $('html, body').animate({
+			        scrollTop: obj.offset().top - offset
+			    }, 750);
+	          
+          return false;
+			  }
+			});
+    });
+  };
+
+  $(document).ready(function () {
+  	$scope.initAnimateScroll();
+  });
 }]);
